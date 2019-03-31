d73512965ebe0682242ea04f5fc38dba191a23ff
Tuan <nvtuan1404@gmail.com>
2019-03-31 22:14:37 +0700

    do du lieu ra

--- MISA.CUKCUK/MISA.CUKCUK.CustomerPage/wwwroot/Scripts/Views/index.js
+++ MISA.CUKCUK/MISA.CUKCUK.CustomerPage/wwwroot/Scripts/Views/index.js
@@ -6,8 +6,6 @@
             url: "https://localhost:44370/api/Customer/Get",
             method: "GET",
             success: function (data) {
-                debugger
-                //var dataCustomer = JSON.stringify(data); // Chuyển dữ liệu trả về từ Object sang chuỗi JSON
                 CusJS.builDataIntoTable(data);
             },
             error: function () {
@@ -26,22 +24,20 @@ var CusJS = Object.create({
     // Hàm build dữ liệu lên table
     // Created by NVTuan ngày 29/3/2019
     builDataIntoTable: function (data) {
-        debugger
         var table = $('#tbodyCustomer');
         table.html('');
         var column = $('#tableCustomer thead tr th'); // Lấy thông tin các cột dữ liệu
         var fieldData = []; // Khai báo một mảng để lưu các fieldData
-        $.each(column,function (index, item) {
-            var itemFieldData = $(item).attr('fieldData'); // Thấy trường nào có fieldData thì lấy tên thuộc tính của trường đó
+        $.each(column, function (index, item) {
+            var itemFieldData = $(item).attr('fieldData'); // Lấy giá trị của các fielData
             fieldData.push(itemFieldData);
         });
         data.forEach(function (item, index) {
-            debugger
             var htmlItem = [];
             htmlItem.push('<tr>'); // Thêm thẻ mở td của một hàng vào mảng htmlItem
-            fieldData.forEach(function (value, index) { // Duyệt lần lượt các dòng
-                var valueCustomer = item[value]; // Lấy giá trị mỗi cột
-                htmlItem.push('<td>' + valueCustomer + '<td>');
+            fieldData.forEach(function (valueField, indexField) { // Duyệt mang fieldData
+                var value = item[valueField]; // Lấy giá trị của từng cột
+                htmlItem.push('<td>' + value + '</td>'); // Push vào mảng htmlItem
             });
             htmlItem.push('</tr>');
             table.append(htmlItem.join(""));
