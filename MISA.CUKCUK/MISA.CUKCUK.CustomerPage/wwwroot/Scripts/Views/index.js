5f9730cdf3c4d2a4f130fbe78572306e2d466447
nvtuan1404 <nvtuan1404@gmail.com>
2019-04-01 18:13:50 +0700

    update code cuk cuk

--- MISA.CUKCUK/MISA.CUKCUK.CustomerPage/wwwroot/Scripts/Views/index.js
+++ MISA.CUKCUK/MISA.CUKCUK.CustomerPage/wwwroot/Scripts/Views/index.js
@@ -1,4 +1,5 @@
 ﻿class CustomerJS {
+
     // Hàm loadData để gọi api lấy dữ liệu đổ về
     // Created by NVTuan ngày 29/3/2019
     loadData() {
@@ -6,41 +7,77 @@
             url: "https://localhost:44370/api/Customer/Get",
             method: "GET",
             success: function (data) {
-                CusJS.builDataIntoTable(data);
+                CusObject.builDataIntoTable(data);
             },
             error: function () {
                 console.log("Có lỗi khi lấy dữ liệu");
             }
         });
     }
+
+    // Hàm thêm một row
+    //Created by NVTuan ngày 1/4/2019
+    addRow() {
+        var id = $('.rowSelected').attr('customerId'); // Lấy ra biến id của row
+        alert(id);
+    }
+
+    // Hàm nhân bản một row
+    //Created by NVTuan ngày 1/4/2019
+    dupplicateRow() {
+        var id = $('.rowSelected').attr('customerId'); // Lấy ra biến id của row
+        alert(id);
+    }
+
+    // Hàm sửa một row
+    //Created by NVTuan ngày 1/4/2019
+    editRow() {
+        var id = $('.rowSelected').attr('customerId'); // Lấy ra biến id của row
+        alert(id);
+    }
+
+    // Hàm xóa một row
+    //Created by NVTuan ngày 1/4/2019
+    deleteRow() {
+        var id = $('.rowSelected').attr('customerId'); // Lấy ra biến id của row
+        alert(id);
+    }
+
+    // Hàm nạp dữ liệu
+    //Created by NVTuan ngày 1/4/2019
+    refreshData() {
+        var id = $('.rowSelected').attr('customerId'); // Lấy ra biến id của row
+        alert(id);
+    }
+
 }
 
 $(document).ready(function () {
-    var CJS = new CustomerJS();
-    CJS.loadData();
-});
+    var CJS = new CustomerJS(); // Khởi tạo đối tượng
+    CJS.loadData();// Gọi hàm load dữ liệu lên bảng
+    $('#tableCustomer').on('click', '#tbodyCustomer tr', CusObject.rowSelected); // Gọi hàm chọn một bản ghi 
+    //$('#btnAdd').on('click', CJS.addRow); // Gọi hàm thêm một hàng
+    $('#btnDupplicate').on('click', CJS.dupplicateRow); // Gọi hàm nhân bản một hàng
+    $('#btnEdit').on('click', CJS.editRow); // Gọi hàm sửa một hàng
+    $('#btnDelete').on('click', CJS.deleteRow); // Gọi hàm xóa một hàng
+    $('#btnRefresh').on('click', CJS.refreshData); // Gọi hàm nạp dữ liệu
+    $('#btnAdd').click(function () {
+        dialog = $("#dialogCustomer").dialog({
+            autoOpen: true,
+            height: 334,
+            width: 680,
+            modal: true,
+            buttons: {
+                "Cất": function () {
+                },
+                "Đóng": function () {
+                    dialog.dialog("close");
+                }
+            },
+            close: function () {
+               
+            }
 
-var CusJS = Object.create({
-    // Hàm build dữ liệu lên table
-    // Created by NVTuan ngày 29/3/2019
-    builDataIntoTable: function (data) {
-        var table = $('#tbodyCustomer');
-        table.html('');
-        var column = $('#tableCustomer thead tr th'); // Lấy thông tin các cột dữ liệu
-        var fieldData = []; // Khai báo một mảng để lưu các fieldData
-        $.each(column, function (index, item) {
-            var itemFieldData = $(item).attr('fieldData'); // Lấy giá trị của các fielData
-            fieldData.push(itemFieldData);
         });
-        data.forEach(function (item, index) {
-            var htmlItem = [];
-            htmlItem.push('<tr>'); // Thêm thẻ mở td của một hàng vào mảng htmlItem
-            fieldData.forEach(function (valueField, indexField) { // Duyệt mang fieldData
-                var value = item[valueField]; // Lấy giá trị của từng cột
-                htmlItem.push('<td>' + value + '</td>'); // Push vào mảng htmlItem
-            });
-            htmlItem.push('</tr>');
-            table.append(htmlItem.join(""));
-        });
-    }
+    });
 });
